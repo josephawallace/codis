9aad299779adc817b810f64b17bcd805dacfbd99
Joseph A. Wallace <jwallace@genesistrading.com>
2022-12-11 21:57:44 -0500

    added ephemeral cli client for testing

--- cmd/client.go
+++ cmd/client.go
@@ -5,13 +5,10 @@ import (
 	"errors"
 	"github.com/milquellc/codis/network"
 	"github.com/milquellc/codis/proto/pb"
-	"os"
-
-	"github.com/AlecAivazis/survey/v2"
-	"github.com/AlecAivazis/survey/v2/terminal"
-	rpc "github.com/libp2p/go-libp2p-gorpc"
-	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/milquellc/codis/protocols"
 	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/proto"
+	"time"
 )
 
 // startClientCmd starts a client node. This node reaches out to its configured "host" via RPC call to initiate a network
@@ -28,167 +25,83 @@ convenient though, which is why we have this--to send test client commands from 
 
 			client := network.NewPeer(ctx, cfg)
 
-			rpcClient, err := client.StartRPCClient(ctx, cfg.Host)
-			if err != nil {
-				logger.Fatal(err)
-			} else {
-				logger.Debug("started RPC client")
-			}
-
 			logger.Info("client is running! listening at %s", client.ListenAddrs())
 
-			hostInfo, err := peer.AddrInfoFromString(cfg.Host)
-			if err != nil {
-				logger.Fatal(err)
+			time.Sleep(time.Second * 5)
+
+			var servicePeerList []string
+			for _, peerId := range client.Host.Peerstore().Peers() {
+				if peerId.String() == client.Host.ID().String() {
+					continue // don't self-dial
+				}
+
+				// service peers must be online and implement the protocols checked below
+				if _, err := client.Host.NewStream(context.Background(), peerId, "/ipfs/ping/1.0.0"); err != nil {
+					continue
+				}
+				if pid, err := client.Host.Peerstore().FirstSupportedProtocol(peerId, protocols.KeygenPId); err != nil || pid != protocols.KeygenPId {
+					continue
+				}
+				if pid, err := client.Host.Peerstore().FirstSupportedProtocol(peerId, protocols.SignPId); err != nil || pid != protocols.SignPId {
+					continue
+				}
+				servicePeerList = append(servicePeerList, peerId.String())
 			}
 
-			go clientLoop(client, rpcClient, hostInfo.ID)
+			topic, _ := cmd.Flags().GetString("topic")
+
+			if topic == protocols.KeygenTopic {
+				alg, _ := cmd.Flags().GetString("algorithm")
+				count, _ := cmd.Flags().GetInt32("count")
+				threshold, _ := cmd.Flags().GetInt32("threshold")
+
+				var party []string
+				for _, servicePeer := range servicePeerList {
+					if len(party) < int(count) {
+						party = append(party, servicePeer)
+					}
+				}
+
+				if len(party) < int(count) {
+					logger.Fatal(errors.New("not enough service nodes available to complete the request"))
+				}
+
+				keygenArgs := pb.KeygenArgs{
+					Count:     count,
+					Threshold: threshold,
+					Alg:       alg,
+					Party:     party,
+				}
+
+				data, err := proto.Marshal(&keygenArgs)
+				if err != nil {
+					logger.Fatal(err)
+				}
+
+				if err = client.Publish(ctx, topic, data); err != nil {
+					logger.Fatal(err)
+				}
+			}
+
 			client.RunUntilCancel()
 		},
 	}
 
+	cmd.Flags().StringP("topic", "T", "", "topic to publish on")
+	cmd.Flags().StringP("message", "m", "", "message to signed (in hex)")
+	cmd.Flags().StringP("algorithm", "a", "ecdsa", "algorithm the key should work with")
+	cmd.Flags().Int32P("count", "c", 0, "total number of peers that should receive a share of the secret")
+	cmd.Flags().Int32P("threshold", "t", 0, "number of peers that need to collaborate to use the secret")
+
+	cmd.MarkFlagsMutuallyExclusive("message", "algorithm")
+	cmd.MarkFlagsMutuallyExclusive("message", "count")
+	cmd.MarkFlagsMutuallyExclusive("message", "threshold")
+
+	cmd.MarkFlagsRequiredTogether("algorithm", "count", "threshold")
+
+	if err := cmd.MarkFlagRequired("topic"); err != nil {
+		logger.Fatal(err)
+	}
+
 	return cmd
-}
-
-const (
-	signAction   = "sign"
-	keygenAction = "keygen"
-)
-
-// clientLoop defines the interface for the Codis client. The loop waits for input on all the args needed to run the
-// different protocols.
-func clientLoop(client *network.Peer, rpcClient *rpc.Client, hostId peer.ID) {
-	for {
-		// the start prompt is used so that we have a clean idle state, we can read the current peer list at the moment
-		// we are ready, and so that the logs for different requests have a point of separation.
-		var start string
-		startPrompt := &survey.Input{Message: "Your Codis Client is live, press enter to see the menu..."}
-		if err := survey.AskOne(startPrompt, &start); err != nil {
-			logger.Error(err)
-			continue
-		}
-
-		// ideally, this list would only consist of service peers on the network so that a user can't start a stream
-		// with a node that doesn't support the protocol. however, there is no apparent way to distinguish a bootstrap/
-		// service/client node from each other without implementing a custom identify protocol.
-		var servicePeerList []string
-		for _, p := range client.Host.Peerstore().Peers() {
-			if p.String() == client.Host.ID().String() { // don't include self in peer list
-				continue
-			}
-			servicePeerList = append(servicePeerList, p.String())
-		}
-
-		startResponse := menuPrompt(hostId, servicePeerList)
-
-		var response interface{}
-		switch startResponse.Action {
-		case keygenAction:
-			protocolArgs := pb.KeygenArgs{
-				Party:     startResponse.Party,
-				Alg:       startResponse.Alg,
-				Count:     int32(len(startResponse.Party)),
-				Threshold: int32(startResponse.Threshold),
-			}
-			protocolReply := pb.KeygenReply{}
-
-			if err := rpcClient.Call(hostId, "KeygenService", "Keygen", &protocolArgs, &protocolReply); err != nil {
-				logger.Error(err)
-			}
-		case signAction:
-			response = signPrompt()
-
-			protocolArgs := pb.SignArgs{
-				Party:     startResponse.Party,
-				Alg:       startResponse.Alg,
-				Count:     int32(len(startResponse.Party)),
-				Threshold: int32(startResponse.Threshold),
-				Message:   response.(signAnswers).Message,
-			}
-			protocolReply := pb.SignReply{}
-
-			if err := rpcClient.Call(hostId, "SignService", "Sign", &protocolArgs, &protocolReply); err != nil {
-				logger.Error(err)
-			}
-		default:
-			logger.Error(errors.New("invalid action"))
-		}
-	}
-}
-
-// startAnswers defines the responses that are necessary for any further operation (in other words, the common parameters
-// for each protocol that the client can invoke on its host)
-type startAnswers struct {
-	Action    string
-	Alg       string
-	Party     []string
-	Threshold int
-}
-
-// menuPrompt fills out a startAnswers to form the basis of a client's request to its host node
-func menuPrompt(clientHost peer.ID, servicePeerList []string) startAnswers {
-	qs := []*survey.Question{
-		{
-			Name:   "action",
-			Prompt: &survey.Select{Message: "Which protocol do you want to run:", Options: []string{"sign", "keygen"}},
-		},
-		{
-			Name: "alg",
-			Prompt: &survey.Select{
-				Message: "Select DSA for signing/generating compatible key:",
-				Options: []string{"ecdsa", "eddsa"},
-			},
-		},
-		{
-			Name: "party",
-			Prompt: &survey.MultiSelect{
-				Message: "Select the participating nodes:",
-				Options: servicePeerList,
-				Default: []string{clientHost.String()},
-			},
-		},
-		{
-			Name: "threshold",
-			Prompt: &survey.Input{
-				Message: "How many peers need to collaborate to use the secret:",
-			},
-		},
-	}
-
-	var answers startAnswers
-	if err := survey.Ask(qs, &answers); err != nil {
-		if err == terminal.InterruptErr {
-			os.Exit(0)
-		}
-		logger.Error(err)
-	}
-
-	return answers
-}
-
-// signAnswers represents the info that supplements startAnswers to form a complete signature request for a service node
-type signAnswers struct {
-	Message string
-}
-
-// signPrompt fills out a signAnswers that is used as part of a client's request to its host to generate a signature
-func signPrompt() signAnswers {
-	qs := []*survey.Question{
-		{
-			Name: "message",
-			Prompt: &survey.Input{
-				Message: "Input the message to be signed (in hex):",
-			},
-		},
-	}
-
-	var answers signAnswers
-	if err := survey.Ask(qs, &answers); err != nil {
-		if err == terminal.InterruptErr {
-			os.Exit(0)
-		}
-		logger.Error(err)
-	}
-
-	return answers
 }
